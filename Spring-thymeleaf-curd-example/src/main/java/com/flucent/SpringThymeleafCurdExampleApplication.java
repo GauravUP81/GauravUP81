333342ddd0951e9f61462afc7bef44dc6eb54999
KINSHUK98 <kinshukmaity54@gmail.com>
2022-05-17 19:46:32 +0530

    kinshuk_22

---
+++ Spring-thymeleaf-curd-example/src/main/java/com/flucent/SpringThymeleafCurdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.flucent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafCurdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafCurdExampleApplication.class, args);
+	}
+
+}
---
+++ Spring-thymeleaf-curd-example/src/main/java/com/flucent/dao/UpdateStudentDTO.java
@@ -0,0 +1,14 @@
+package com.flucent.dao;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateStudentDTO extends CreateStudentDTO {
+
+    private String id;
+
+}
